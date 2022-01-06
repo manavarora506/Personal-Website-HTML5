1258ef17f91e2a03ce2d657cec83ef4c213a97b8
manavarora <manavarora506@utexas.edu>
2022-01-06 17:25:50 -0600

    added songs

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,9 +1,14 @@
 var tracks = ['6hzWWZjqHcoI2KIWpldo9v', '3Y4rUyw7XBCK6hGHCOt6rp','5WoBAyXaOnKEfvsZocrPeT', '55mvtucws4Mnro27744t9X?si=3849016cc3f042e4', '7kRKlFCFLAUwt43HWtauhX', '7a8GawTVgHZvBLQOh46he7',
 '4dj1qglNo2j45K8z2sDFEY', '6FfLg6FFqhCsrFOaHIGkg0', '44CZRkOxv7UItaAUmh8PgN', '7lPjS6Yd4lRk4BsboDsm1H', '5G1Xj9wn0TZRf7TSbiIY8M', '0lO5zEKJ60lvz8X192p3WK', '3REELyDyt5SPKX2387Zubv',
- '5QTxFnGygVM4jFQiBovmRo', '4d9RTWdrFLVAGhdzvqxkwn', '15EPc80XuFrb2LmOzGjuRg', '2jiPHbFX5XM8TjaTfYJ0eo']
+ '5QTxFnGygVM4jFQiBovmRo', '4d9RTWdrFLVAGhdzvqxkwn', '15EPc80XuFrb2LmOzGjuRg', '2jiPHbFX5XM8TjaTfYJ0eo', '6VFKuuqSbA1GpMEosUgTwQ', '5PNvgiKSwMdjBsDsgFCFLX', '5AiNZnMDCWwujIENPj9PV9','5k7VKj1Xwy5DjO4B0PdAOb', '26YrTYQEgmDvx4vlHLLAsT', 
+ '3A9vIxzGBjEfqmDK7H9exS', '2nTsKOXIVGDf2iPeVQO2Gm', '15ea10YpJIl3mJq75yzqsD','0ofHAoxe9vBkTCp2UQIavz','4P5KoWXOxwuobLmHXLMob', '40riOy7x9W7GXjyGp4pjAv','5BIMPccDwShpXq784RJlJp','6cr6UDpkjEaMQ80OjWqEBQ','1RJeiAIwR9pZBgJA8ndZLL','6Vjk8MNXpQpi0F4BefdTyq', 
+ '5uvosCdMlFdTXhoazkTI5R','4Li2WHPkuyCdtmokzW2007','4fzsfWzRhPawzqhX8Qt9F3','6Bg7MznA9X0dIhlAsLyBYj','4RVbK6cV0VqWdpCDcx3hiT']
 function getRandomTrack() {
     return "https://open.spotify.com/embed/track/" + tracks[Math.floor(Math.random() * tracks.length)];
 }
+
+
+
 
 
 
